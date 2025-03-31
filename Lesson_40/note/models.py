9c0579a46cadad560847dfb2b130324e4173f0ec
mrGarmr <nvova@i.ua>
2025-03-31 09:01:14 -0400

    start

---
+++ Lesson_40/note/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    reminder = models.DateTimeField(null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='notes')
+
+    def __str__(self):
+        return self.title
